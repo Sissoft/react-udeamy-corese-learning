55dbfdc05bae6d4cce146ecb46ec76b9df5cca19
unknown <Sissoft@sis21age@gmail.com>
2026-04-28 10:12:34 +0300

    Add files to git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Learn react on sis</h1>
+      <p>this what we try to learn please </p>
+    </>
+  );
+}
+
+export default App;
